7d87a6dfd80c51f0e6909830f9397a98bac911c1
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:35:40 +0700

    addTokenRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/TokenRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Request
+{
+    public class TokenRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string RoleName { get; set; }
+    }
+}
