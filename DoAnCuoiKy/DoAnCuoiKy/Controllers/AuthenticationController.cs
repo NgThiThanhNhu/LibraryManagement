c7777f5462edbdbbb91bc9ddb3dfc45efafcdf18
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:31:32 +0700

    addAuthenticationController

--- DoAnCuoiKy/DoAnCuoiKy/Controllers/AuthenticationController.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/AuthenticationController.cs
@@ -1,10 +1,12 @@
 ﻿using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 using DoAnCuoiKy.Service.IService.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnCuoiKy.Controllers
 {
+    [Authorize]//không cần token
     [Route("api/[controller]")]
     [ApiController]
     public class AuthenticationController : ControllerBase
@@ -14,11 +16,19 @@ namespace DoAnCuoiKy.Controllers
         {
             _authenticationService = authenticationService;
         }
+        [AllowAnonymous] //cho phép truy cập bất kì
         [HttpPost("Login")]
-        public async Task<BaseResponse<LoginResponse>> Login(LoginRequest loginRequest) { 
-        BaseResponse<LoginResponse> baseResponse = await _authenticationService.Login(loginRequest);
+        public async Task<BaseResponse<LoginResponse>> Login(LoginRequest loginRequest)
+        {
+            BaseResponse<LoginResponse> baseResponse = await _authenticationService.Login(loginRequest);
             return baseResponse;
         }
-
+        [AllowAnonymous]
+        [HttpPost("Register")]
+        public async Task<BaseResponse<RegisterResponse>> Register(RegisterRequest registerRequest)
+        {
+            BaseResponse<RegisterResponse> baseResponse = await _authenticationService.Register(registerRequest);
+            return baseResponse;
+        }
     }
 }
