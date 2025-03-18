48074066a17556f9dac60d7b44203ef8bc8492f3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:36:05 +0700

    addRegisterResponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/RegisterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Response
+{
+    public class RegisterResponse
+    {
+        public string Name { get; set; }
+        //public string Token { get; set; 
+        
+    }
+}
