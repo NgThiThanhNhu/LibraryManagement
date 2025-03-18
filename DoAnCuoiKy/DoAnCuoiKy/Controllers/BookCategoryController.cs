d812dc6c5ef396ce032525720902a8aa9f559bbb
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:32:04 +0700

    updateBookCategoryController

--- DoAnCuoiKy/DoAnCuoiKy/Controllers/BookCategoryController.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/BookCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 using DoAnCuoiKy.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,35 +19,37 @@ namespace DoAnCuoiKy.Controllers
         }
 
         [HttpPost("AddBookCategory")]
-        public  async Task<BaseResponse<BookCategoryResponse>> addBookCategory(BookCategoryRequest bookCategoryRequest)
+        [Authorize(Roles = "Admin")]
+        public async Task<BaseResponse<BookCategoryResponse>> addBookCategory(BookCategoryRequest bookCategoryRequest)
         {
             //gọi lại
-            BaseResponse<BookCategoryResponse> response = await  _bookCategoryService.AddBookCategory(bookCategoryRequest);
+            BaseResponse<BookCategoryResponse> response = await _bookCategoryService.AddBookCategory(bookCategoryRequest);
             return response;
         }
 
         [HttpGet("GetAllBookCategory")]
+        [Authorize(Roles = "Admin, User")]
         public async Task<BaseResponse<List<BookCategoryResponse>>> getAllBookCategory()
         {
             BaseResponse<List<BookCategoryResponse>> responses = await _bookCategoryService.GetAllBookCategory();
             return responses;
         }
-
         [HttpGet("GetBookCategoryById/{id}")]
+        [Authorize(Roles = "Admin, User")]
         public async Task<BaseResponse<BookCategoryResponse>> getBookCategoryById(Guid id)
         {
             BaseResponse<BookCategoryResponse> response = await _bookCategoryService.GetBookCategoryById(id);
             return response;
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPost("UpdateBookCategory/{id}")]
         public async Task<BaseResponse<BookCategoryResponse>> updateBookCategory(Guid id, BookCategoryRequest categoryRequest)
         {
             BaseResponse<BookCategoryResponse> response = await _bookCategoryService.UpdateBookCategory(id, categoryRequest);
             return response;
         }
-
         [HttpPost("DeleteBookCategory/{id}")]
+        [Authorize(Roles = "Admin")]
         public async Task<BaseResponse<BookCategoryResponse>> deleteBookCategory(Guid id)
         {
             BaseResponse<BookCategoryResponse> response = await _bookCategoryService.DeleteBookCategory(id);
