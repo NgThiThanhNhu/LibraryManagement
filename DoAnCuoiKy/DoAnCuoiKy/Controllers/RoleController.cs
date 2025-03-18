0a9d0f0cf72868917ffb4f69804f33987da2fbd7
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:33:33 +0700

    updateRoleController

--- DoAnCuoiKy/DoAnCuoiKy/Controllers/RoleController.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/RoleController.cs
@@ -1,10 +1,12 @@
 ﻿using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 using DoAnCuoiKy.Service.IService.Usermanage;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnCuoiKy.Controllers
 {
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RoleController : ControllerBase
@@ -15,12 +17,16 @@ namespace DoAnCuoiKy.Controllers
             _roleService = roleService;
         }
         [HttpPost("AddRole")]
+        //[AllowAnonymous]
+        [Authorize(Roles = "Admin")]
         public async Task<BaseResponse<RoleResponse>> addRole(RoleRequest roleRequest)
         {
             BaseResponse<RoleResponse> response = await _roleService.AddRole(roleRequest);
             return response;
         }
         [HttpGet("GetAllRole")]
+        //[AllowAnonymous]
+        [Authorize(Roles = "Admin")]
         public async Task<BaseResponse<List<RoleResponse>>> getAllRole()
         {
             BaseResponse<List<RoleResponse>> response = await _roleService.GetAllRole();
