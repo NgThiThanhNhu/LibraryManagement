79299220b1db89ed24454c7981f8d73da85773c1
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:35:19 +0700

    addRgisterRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Request
+{
+    public class RegisterRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+    }
+}
