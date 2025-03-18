a994efc8ee42420c29f9977d44108a8bc093be67
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-03-18 15:38:28 +0700

    updateJwtProgram

--- DoAnCuoiKy/DoAnCuoiKy/Program.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Program.cs
@@ -6,9 +6,18 @@ using DoAnCuoiKy.Service.IService;
 using DoAnCuoiKy.Service.IService.InformationLibrary;
 using DoAnCuoiKy.Service.IService.Usermanage;
 using DoAnCuoiKy.Service.Usermanage;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DoAnCuoiKy.Service.IService.Authentication;
+using DoAnCuoiKy.Service.Authentication;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
 
 builder.Services.AddCors(options =>
 {
@@ -20,6 +29,24 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
         });
 });
+
+//đăng ký dịch vụ xác thực Jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            RoleClaimType = ClaimTypes.Role
+        };
+    });
+builder.Services.AddAuthorization();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -35,6 +62,9 @@ builder.Services.AddTransient<IBookService, BookService>();
 builder.Services.AddTransient<IBookChapterService, BookChapterService>();
 builder.Services.AddTransient<IBookItemService, BookItemService>();
 builder.Services.AddTransient<IRoleService, RoleService>();
+
+//đăng ký autheticationService 
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 var app = builder.Build();
 //connect với frontend
 app.UseCors("AllowReactApp"); //kích hoạt CORS
@@ -48,7 +78,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+//kích hoạt jwt
+
 
 app.MapControllers();
 
