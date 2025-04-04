713cb1f96746b68bed75277e7de7d6cde9af46ed
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-04 17:32:17 +0700

    addShelf

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Shelf.cs
@@ -0,0 +1,12 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class Shelf
+    {
+        public Guid Id { get; set; }
+        public string ShelfName { get; set; }
+        public int NumberOfSections { get; set; }
+        public Guid BookshelfId { get; set; }  // Liên kết đến Tủ sách chứa Kệ sách này
+        public BookShelf Bookshelf { get; set; }  // Mối quan hệ với Tủ sách
+        public List<ShelfSection> Sections { get; set; }
+    }
+}
