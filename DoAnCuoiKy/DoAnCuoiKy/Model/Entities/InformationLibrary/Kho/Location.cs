bd7469ae29351a4e709ecc546d61c684926ea2de
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-04 17:31:35 +0700

    addLocationforbookitem

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class Location : LocationBase
+    {
+        public Guid Id { get; set; }
+        public Guid ShelfSection {  get; set; }
+        public ShelfSection Shelf { get; set; }
+        public Guid BookItemId { get; set; }
+        public BookItem BookItem { get; set; }
+        public string Description { get; set; }
+
+    }
+}
