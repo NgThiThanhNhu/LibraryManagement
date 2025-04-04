28fc6bbe84a860ee3300dd85d87180f995cb208e
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-04 17:31:56 +0700

    addLocationbase

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/LocationBase.cs
@@ -0,0 +1,8 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class LocationBase
+    {
+        public string Room {  get; set; }
+        public string Floor { get; set; }
+    }
+}
