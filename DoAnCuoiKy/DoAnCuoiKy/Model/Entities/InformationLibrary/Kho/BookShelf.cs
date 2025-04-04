68d413da2e97dcd33ae3d112226aed36d3c67343
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-04 17:31:09 +0700

    addBookShelf

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/BookShelf.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class BookShelf
+    {
+        public Guid Id { get; set; }
+        public string BookShelfName { get; set; }
+        public List<Shelf> Shelves { get; set; }
+    }
+}
