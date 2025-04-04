6d009f194b18a793754c3bdc278878ebf3655f63
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-04 17:32:38 +0700

    addShelfSection

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/ShelfSection.cs
@@ -0,0 +1,13 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class ShelfSection
+    {
+        public Guid Id { get; set; }
+        public string SectionName { get; set; }
+        public int Capacity { get; set; } //số lượng sách có thể chứa của 1 ô sách
+        public Guid ShelfId { get; set; }
+        public Shelf Shelf { get; set; }
+        //1 ô chứa nhiều bookitem mà location là vị trí của 1 bookitem trong ô suy ra 1 ô sẽ có nhiều location chi tiết của bookitem đó
+        public List<Location> Locations { get; set; }
+    }
+}
