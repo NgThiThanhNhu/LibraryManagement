2f3a32e3e467881afcc055a2cd49f84a13547900
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-06 01:06:30 +0700

    AddBookAuthor

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/BookAuthor.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary
+{
+    public class BookAuthor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> books { get; set; }
+    }
+}
