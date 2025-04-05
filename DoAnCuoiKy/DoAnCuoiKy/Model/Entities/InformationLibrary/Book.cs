fbb0d6501654ba29d4b801914e31eb54c597faca
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-06 01:07:49 +0700

    updateBookAuthor

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Book.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Book.cs
@@ -1,4 +1,5 @@
-﻿using DoAnCuoiKy.Model.Entities.Usermanage;
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using DoAnCuoiKy.Model.Entities.Usermanage;
 using DoAnCuoiKy.Model.Enum.InformationLibrary;
 using System.ComponentModel.DataAnnotations;
 
@@ -9,24 +10,18 @@ namespace DoAnCuoiKy.Model.Entities.InformationLibrary
        
         public Guid? Id { get; set; }
         public string? Title { get; set; }
-        public string? Author { get; set; }
-
         public Guid PublisherId { get; set; }
         public Publisher Publisher { get; set; }
-       
+        public Guid BookAuthorId { get; set; }
+        public BookAuthor? BookAuthor { get; set; }
         public int? YearPublished { get; set; }
         public int? Quantity { get; set; } //số lượng sách nhập
-        //public BookStatus? Status { get; set; }
-
+        public float? TotalPrice { get; set; }
+        public float? UnitPrice { get; set; }
         public Guid? CategoryId { get; set; }
         public BookCategory? Category { get; set; }
-        
-        //public List<BorrowingDetail> borrowingDetails { get; set; }
-        //public List<BookReservation>? Reservations { get; set; }
         public Guid? BookChapterId { get; set; }
         public BookChapter? BookChapter { get; set; }
         public List<BookItem>? bookItems { get; set; }
-
-       
     }
 }
