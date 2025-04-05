ecdab4a7da79c3c51f165f1cd1456c15ee90e36f
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-06 01:08:13 +0700

    createBookImport

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/BookImportTransaction.cs
@@ -0,0 +1,15 @@
+﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+{
+    public class BookImportTransaction : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid BookId { get; set; }
+        public Book book { get; set; }
+        public int Quantity { get; set; }  // Số lượng nhập
+        public DateTime ImportDate { get; set; }
+        public float UnitPrice { get; set; }
+        public float TotalPrice { get; set; }
+       
+        
+    }
+}
