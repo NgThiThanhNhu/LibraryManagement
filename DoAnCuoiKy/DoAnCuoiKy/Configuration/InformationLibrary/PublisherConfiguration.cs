c32f07fe8e54ee7a0aa9a1aaea8916bc4f96088b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-08 17:12:27 +0700

    addPublisherConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/PublisherConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary
+{
+    public class PublisherConfiguration : IEntityTypeConfiguration<Publisher>
+    {
+        public void Configure(EntityTypeBuilder<Publisher> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p=>p.PublisherName)
+                .HasMaxLength(256);
+            builder.Property(p=>p.Address)
+                .HasMaxLength(256);
+            builder.Property(p=>p.Email)
+                .HasMaxLength(256);
+            builder.Property(p=>p.Phone)
+                .HasMaxLength(10);
+            //xây dựng mối quan hệ với bảng book
+            builder.HasMany(p => p.Books)
+                .WithOne(b => b.Publisher)
+                .HasForeignKey(b => b.PublisherId);
+        }
+    }
+}
