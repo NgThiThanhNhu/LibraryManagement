912918a08cce3fbd7d8f4e81b46ef2e6c1ae85b1
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-08 17:11:58 +0700

    addBookAuhtorConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/BookAuthorConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary
+{
+    public class BookAuthorConfiguration : IEntityTypeConfiguration<BookAuthor>
+    {
+        public void Configure(EntityTypeBuilder<BookAuthor> builder)
+        {
+            builder.HasKey(ba => ba.Id);
+
+            builder.Property(ba=>ba.Name)
+                .HasMaxLength(256);
+            //mối quan hệ với bảng book
+            builder.HasMany(ba => ba.books)
+                .WithOne(b => b.BookAuthor)
+                .HasForeignKey(b => b.BookAuthorId);
+
+           
+        }
+    }
+}
