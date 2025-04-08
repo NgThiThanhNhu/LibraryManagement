8db3f20bf07f1ea895b40ba511a58d962033781c
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-08 17:54:14 +0700

    addLocationConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/LocationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.HasKey(l => l.Id);
+            //mối quan hê với ShelfSection
+            builder.HasOne(l => l.ShelfSection)
+                .WithMany(section => section.Locations)
+                .HasForeignKey(l => l.ShelfSectionId);
+            //mối quan hệ với bảng bookitem
+            builder.HasMany(l => l.BookItems)
+                .WithOne(i => i.Location)
+                .HasForeignKey(i => i.LocationId);
+        }
+    }
+}
