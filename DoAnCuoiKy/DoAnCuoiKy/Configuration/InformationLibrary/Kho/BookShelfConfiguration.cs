adbef73dc7c03b4d3e51ccfc05cef6fd48206044
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-08 17:53:54 +0700

    addBookShelfConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookShelfConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class BookShelfConfiguration : IEntityTypeConfiguration<BookShelf>
+    {
+        public void Configure(EntityTypeBuilder<BookShelf> builder)
+        {
+            builder.HasKey(bs => bs.Id);
+            builder.Property(bs=>bs.BookShelfName)
+                .HasMaxLength(256);
+           
+        }
+    }
+}
