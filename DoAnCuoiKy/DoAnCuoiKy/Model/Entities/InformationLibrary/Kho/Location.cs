488a37ddaef86fdf1b7eb95947518fc8e799aab3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-08 17:53:17 +0700

    updateThuocTinhbookitem

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Location.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Location.cs
@@ -3,10 +3,10 @@
     public class Location : LocationBase
     {
         public Guid Id { get; set; }
-        public Guid ShelfSection {  get; set; }
-        public ShelfSection Shelf { get; set; }
-        public Guid BookItemId { get; set; }
-        public BookItem BookItem { get; set; }
+        public Guid ShelfSectionId {  get; set; }
+        public ShelfSection ShelfSection { get; set; }
+        public List<BookItem> BookItems { get; set; } //1 vị trí là địa chỉ củ 1 cuốn sách
+        //nhưng 1 địa chỉ đó có chứa nhiều cuốn sách ví dụ: ở entity shelfsection có thuộc tính Capacity nghĩa là sức chứa của 1 ô section
         public string Description { get; set; }
 
     }
