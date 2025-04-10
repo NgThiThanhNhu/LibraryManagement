98d02748e9bfb1918bbbe7b08819d30917dcd9a7
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-10 23:28:03 +0700

    cap nhat thuoc tinh AuthorId, PublisherId, TotalPrice, unitprice

--- DoAnCuoiKy/DoAnCuoiKy/Model/Request/BookRequest.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/BookRequest.cs
@@ -5,13 +5,13 @@ namespace DoAnCuoiKy.Model.Request
     public class BookRequest
     {
         public string? Title { get; set; }
-        public string? Author { get; set; }
-        public string? Publisher { get; set; }
+        public Guid? AuthorId { get; set; }
+        public Guid? PublisherId { get; set; }
         public int? YearPublished { get; set; }
         public int? Quantity { get; set; } //số lượng sách nhập vào
         public Guid? CategoryId { get; set; }
         public Guid? BookChapterId { get; set; }
-
-
+        public float? TotalPrice { get; set; }
+        public float? UnitPrice { get; set; }
     }
 }
