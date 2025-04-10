3e5d0b75afe54f40046c908bd9b0020c08339a4f
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-10 23:30:30 +0700

    them LocationConfiguration

--- DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/LocationConfiguration.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/LocationConfiguration.cs
@@ -9,14 +9,20 @@ namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
         public void Configure(EntityTypeBuilder<Location> builder)
         {
             builder.HasKey(l => l.Id);
+
             //mối quan hê với ShelfSection
             builder.HasOne(l => l.ShelfSection)
                 .WithMany(section => section.Locations)
                 .HasForeignKey(l => l.ShelfSectionId);
+
             //mối quan hệ với bảng bookitem
             builder.HasMany(l => l.BookItems)
                 .WithOne(i => i.Location)
                 .HasForeignKey(i => i.LocationId);
+
+            builder.Property(l => l.Description)
+                .HasMaxLength(256);
         }
+
     }
 }
