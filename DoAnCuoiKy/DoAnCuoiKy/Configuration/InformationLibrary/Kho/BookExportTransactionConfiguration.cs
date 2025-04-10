37a0b67ef83c04e6784aa1ea502f33a9359f01c7
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-10 23:29:44 +0700

    them BookExportTransactionConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookExportTransactionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class BookExportTransactionConfiguration : IEntityTypeConfiguration<BookExportTransaction>
+    {
+        public void Configure(EntityTypeBuilder<BookExportTransaction> builder)
+        {
+            builder.HasKey(ep => ep.Id);
+            builder.Property(ep => ep.ExportReason)
+                .HasConversion<byte>();
+        }
+    }
+}
