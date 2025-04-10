0f31521e0d25f1c41d5c64794109d5b68a4eef2e
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-10 23:30:13 +0700

    them bookImportTransactionConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookImportTransactionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class BookImportTransactionConfiguration : IEntityTypeConfiguration<BookImportTransaction>
+    {
+        public void Configure(EntityTypeBuilder<BookImportTransaction> builder)
+        {
+            builder.HasKey(ip => ip.Id);
+
+            builder.Property(ip => ip.Quantity);
+
+            builder.Property(ip => ip.UnitPrice);
+
+            builder.Property(ip => ip.TotalPrice);
+        }
+    }
+}
