1afea2acd7a77a913d2d22b165aeeb03d5bb5522
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-12 15:15:17 +0700

    thêm enum ExportReason

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/Kho/ExportReason.cs
@@ -0,0 +1,11 @@
+﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary.Kho
+{
+    public enum ExportReason : byte
+    {
+        Borrow = 0,
+        Transfer = 1,
+        Damaged = 2,
+        Lost = 3,
+        Disposal = 4
+    }
+}
