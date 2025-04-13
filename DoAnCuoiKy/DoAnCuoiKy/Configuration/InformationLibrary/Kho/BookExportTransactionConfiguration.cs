a4f884505c6f193b20ccec91d1d8f457478da8a3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-13 18:15:37 +0700

    NguyenThiThanhNhu-generateBug

--- DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookExportTransactionConfiguration.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookExportTransactionConfiguration.cs
@@ -4,13 +4,4 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
 {
-    public class BookExportTransactionConfiguration : IEntityTypeConfiguration<BookExportTransaction>
-    {
-        public void Configure(EntityTypeBuilder<BookExportTransaction> builder)
-        {
-            builder.HasKey(ep => ep.Id);
-            builder.Property(ep => ep.ExportReason)
-                .HasConversion<byte>();
-        }
-    }
-}
+    public 
