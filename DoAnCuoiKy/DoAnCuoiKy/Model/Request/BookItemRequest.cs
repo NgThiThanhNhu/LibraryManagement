7cf33dbfe53e39dec9dd83293d2a31a016d14998
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-14 20:42:32 +0700

    update BookItemRequest

--- DoAnCuoiKy/DoAnCuoiKy/Model/Request/BookItemRequest.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/BookItemRequest.cs
@@ -4,11 +4,7 @@ namespace DoAnCuoiKy.Model.Request
 {
     public class BookItemRequest
     {
-        //public string? Title { get; set; }
-        //public string? Author { get; set; }
-        //public string? Publisher { get; set; }
-        //public int? YearPublished { get; set; }
-        //public int? Quantity { get; set; }
+        
         public BookStatus? bookStatus { get; set; }
         public Guid? BookCategoryId { get; set; }
         public Guid? BookChapterId { get; set; }
