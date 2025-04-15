090899d7e57f08dfb07411d50237fe2147f85ae8
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-15 22:01:10 +0700

    add BookAuthorController

---
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/BookAuthorController.cs
@@ -0,0 +1,21 @@
+﻿using DoAnCuoiKy.Model.Request;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Service.IService.InformationLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKy.Controllers
+{
+    public class BookAuthorController : ControllerBase
+    {
+        private readonly IBookAuthorService _bookAuthorService;
+        public BookAuthorController(IBookAuthorService bookAuthorService)
+        {
+            _bookAuthorService = bookAuthorService;
+        }
+        public async Task<BaseResponse<BookAuthorResponse>> addBookAuthor(BookAuthorRequest authorRequest)
+        {
+            BaseResponse<BookAuthorResponse> response = await _bookAuthorService.AddBookAuthor(authorRequest);
+            return response;
+        }
+    }
+}
