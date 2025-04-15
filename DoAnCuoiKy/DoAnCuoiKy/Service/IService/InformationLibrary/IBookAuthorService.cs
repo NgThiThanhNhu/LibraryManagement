a21b32969befc450d4eef38a75668dfa6b04a590
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-15 22:02:15 +0700

    Add IBookAuthorService

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookAuthorService.cs
@@ -0,0 +1,11 @@
+﻿using DoAnCuoiKy.Model.Request;
+using DoAnCuoiKy.Model.Response;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary
+{
+    public interface IBookAuthorService
+    {
+        Task<BaseResponse<BookAuthorResponse>> AddBookAuthor(BookAuthorRequest authorRequest);
+        
+    }
+}
