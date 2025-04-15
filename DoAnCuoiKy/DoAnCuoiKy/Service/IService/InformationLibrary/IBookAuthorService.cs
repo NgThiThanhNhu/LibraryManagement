df75fb43500c70ca3011cfc5cf5b016a938f107d
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-15 22:16:27 +0700

    add GetAllBookAuthor IService

--- DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookAuthorService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookAuthorService.cs
@@ -6,6 +6,10 @@ namespace DoAnCuoiKy.Service.IService.InformationLibrary
     public interface IBookAuthorService
     {
         Task<BaseResponse<BookAuthorResponse>> AddBookAuthor(BookAuthorRequest authorRequest);
+        Task<BaseResponse<List<BookAuthorResponse>>> GetAllBookAuthor();
+        Task<BaseResponse<BookAuthorResponse>> GetBookAuthorById(Guid id);
+        Task<BaseResponse<BookAuthorResponse>> UpdateBookAuthor(Guid Id, BookAuthorRequest authorRequest);
+        Task<BaseResponse<BookAuthorResponse>> DeleteBookAuthor(Guid Id);
         
     }
 }
