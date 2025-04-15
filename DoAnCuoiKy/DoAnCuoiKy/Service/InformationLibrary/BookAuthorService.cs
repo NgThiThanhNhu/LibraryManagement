6c52236c2a323c9961c876416a059016e57dc74d
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-15 22:15:58 +0700

    add GetAllBookAuthor

--- DoAnCuoiKy/DoAnCuoiKy/Service/InformationLibrary/BookAuthorService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/InformationLibrary/BookAuthorService.cs
@@ -3,6 +3,7 @@ using DoAnCuoiKy.Model.Entities.InformationLibrary;
 using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 using DoAnCuoiKy.Service.IService.InformationLibrary;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnCuoiKy.Service.InformationLibrary
 {
@@ -34,6 +35,35 @@ namespace DoAnCuoiKy.Service.InformationLibrary
             response.message = "Thêm dữ liệu thành công";
             response.data = bookAuthorResponse;
             return response;
+        }
+
+        public Task<BaseResponse<BookAuthorResponse>> DeleteBookAuthor(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BaseResponse<List<BookAuthorResponse>>> GetAllBookAuthor()
+        {
+            BaseResponse<List<BookAuthorResponse>> response = new BaseResponse<List<BookAuthorResponse>>();
+            List<BookAuthorResponse> bookAuthorResponse = await _context.bookAuthors.Where(x => x.IsDeleted == false).Select(x => new BookAuthorResponse
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToListAsync();
+            response.IsSuccess = true;
+            response.message = "Lấy list thành công";
+            response.data = bookAuthorResponse;
+            return response;
+        }
+
+        public Task<BaseResponse<BookAuthorResponse>> GetBookAuthorById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<BookAuthorResponse>> UpdateBookAuthor(Guid Id, BookAuthorRequest authorRequest)
+        {
+            throw new NotImplementedException();
         }
     }
 }
