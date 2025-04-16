022cedfcbe12f58bf987089dfe81b4c092a6f2e2
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-16 16:07:27 +0700

    update IPublisherService

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IPublisherService.cs
@@ -0,0 +1,14 @@
+﻿using DoAnCuoiKy.Model.Request;
+using DoAnCuoiKy.Model.Response;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary
+{
+    public interface IPublisherService
+    {
+        Task<BaseResponse<PublisherResponse>> AddPublisher(PublisherRequest publisherRequest);
+        Task<BaseResponse<List<PublisherResponse>>> GetAllPublisher();
+        Task<BaseResponse<PublisherResponse>> GetPublisherById(Guid id);
+        Task<BaseResponse<PublisherResponse>> UpdatePublisher(Guid id, PublisherRequest publisherRequest);
+        Task<BaseResponse<PublisherResponse>> DeletePublisher(Guid id);
+    }
+}
