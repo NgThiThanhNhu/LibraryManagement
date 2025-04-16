66c1d16ef2671ed07daddfa8a8f23b11717a4a0a
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-16 16:04:12 +0700

    add PublisherRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/PublisherRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace DoAnCuoiKy.Model.Request
+{
+    public class PublisherRequest
+    {
+        public string? PublisherName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
