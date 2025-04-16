5b98401d392c59b5513f9c19ec938461762ae6b1
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-16 16:00:54 +0700

    Add UpdateBookAuthor GetBookAuthorById DeleteBookAuthor

--- DoAnCuoiKy/DoAnCuoiKy/Controllers/BookAuthorController.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/BookAuthorController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnCuoiKy.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class BookAuthorController : ControllerBase
     {
         private readonly IBookAuthorService _bookAuthorService;
@@ -12,9 +14,34 @@ namespace DoAnCuoiKy.Controllers
         {
             _bookAuthorService = bookAuthorService;
         }
+        [HttpPost("AddBookAuthor")]
         public async Task<BaseResponse<BookAuthorResponse>> addBookAuthor(BookAuthorRequest authorRequest)
         {
             BaseResponse<BookAuthorResponse> response = await _bookAuthorService.AddBookAuthor(authorRequest);
+            return response;
+        }
+        [HttpGet("GetAllBookAuthor")]
+        public async Task<BaseResponse<List<BookAuthorResponse>>> getAllBookAuthor()
+        {
+            BaseResponse<List<BookAuthorResponse>> response = await _bookAuthorService.GetAllBookAuthor();
+            return response;
+        }
+        [HttpGet("GetBookAuthorById/{id}")]
+        public async Task<BaseResponse<BookAuthorResponse>> getBookAuthorById(Guid id)
+        {
+            BaseResponse<BookAuthorResponse> response = await _bookAuthorService.GetBookAuthorById(id);
+            return response;
+        }
+        [HttpPost("DeleteBookAuthor/{id}")]
+        public async Task<BaseResponse<BookAuthorResponse>> deleteBookAuthor(Guid id)
+        {
+            BaseResponse<BookAuthorResponse> response = await _bookAuthorService.DeleteBookAuthor(id);
+            return response;
+        }
+        [HttpPost("UpdateBookAuthor/{id}")]
+        public async Task<BaseResponse<BookAuthorResponse>> updateBookAuthor(Guid id, BookAuthorRequest authorRequest)
+        {
+            BaseResponse<BookAuthorResponse> response = await _bookAuthorService.UpdateBookAuthor(id, authorRequest);
             return response;
         }
     }
