c6ef9d43abadaa2b51a86ed017c8cebc2cae5825
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-16 16:07:06 +0700

    update IBookAuthorService

--- DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookAuthorService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookAuthorService.cs
@@ -8,8 +8,8 @@ namespace DoAnCuoiKy.Service.IService.InformationLibrary
         Task<BaseResponse<BookAuthorResponse>> AddBookAuthor(BookAuthorRequest authorRequest);
         Task<BaseResponse<List<BookAuthorResponse>>> GetAllBookAuthor();
         Task<BaseResponse<BookAuthorResponse>> GetBookAuthorById(Guid id);
-        Task<BaseResponse<BookAuthorResponse>> UpdateBookAuthor(Guid Id, BookAuthorRequest authorRequest);
-        Task<BaseResponse<BookAuthorResponse>> DeleteBookAuthor(Guid Id);
+        Task<BaseResponse<BookAuthorResponse>> UpdateBookAuthor(Guid id, BookAuthorRequest authorRequest);
+        Task<BaseResponse<BookAuthorResponse>> DeleteBookAuthor(Guid id);
         
     }
 }
