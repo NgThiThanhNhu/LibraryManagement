f5a2e450e108f6111733b21fc53353f4b8ec31a3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-16 16:04:31 +0700

    add PublisherResponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/PublisherResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace DoAnCuoiKy.Model.Response
+{
+    public class PublisherResponse
+    {
+        public Guid? Id { get; set; }
+        public string? PublisherName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
