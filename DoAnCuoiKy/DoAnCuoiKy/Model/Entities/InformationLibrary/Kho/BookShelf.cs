5f2e846ada2cfea29455499994fe81f568144779
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:23:56 +0700

    add NumberOfShelves

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/BookShelf.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/BookShelf.cs
@@ -2,8 +2,9 @@
 {
     public class BookShelf : BaseEntity
     {
-        public Guid Id { get; set; }
-        public string BookShelfName { get; set; }
-        public List<Shelf> Shelves { get; set; }
+        public Guid? Id { get; set; }
+        public string? BookShelfName { get; set; }
+        public int? NumberOfShelves { get; set; } // số kệ trong 1 tủ
+        public List<Shelf>? Shelves { get; set; }
     }
 }
