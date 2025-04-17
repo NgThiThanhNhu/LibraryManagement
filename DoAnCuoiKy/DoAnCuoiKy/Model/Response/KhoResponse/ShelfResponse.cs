6296395afbabcd523ccc76982545d8f41def8f8b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:25:12 +0700

    add ShelfResponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/KhoResponse/ShelfResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DoAnCuoiKy.Model.Response.KhoResponse
+{
+    public class ShelfResponse
+    {
+        public Guid? Id { get; set; }
+        public string? ShelfName { get; set; }
+        public int? NumberOfSections { get; set; }
+        public string? BookshelfName { get; set; }  // Liên kết đến Tủ sách chứa Kệ sách này
+    }
+}
