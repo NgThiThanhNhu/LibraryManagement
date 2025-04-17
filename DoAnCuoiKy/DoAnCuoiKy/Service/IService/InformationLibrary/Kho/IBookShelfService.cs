ab1f6a07ea06a21c56720528c6fd3cce8c3b648c
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:27:00 +0700

    add IBookShelfService

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IBookShelfService.cs
@@ -0,0 +1,15 @@
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
+{
+    public interface IBookShelfService
+    {
+        Task<BaseResponse<BookShelfResponse>> AddBookShelf(BookShelfRequest bookShelfRequest);
+        Task<BaseResponse<List<BookShelfResponse>>> GetAllBookShelf();
+        Task<BaseResponse<BookShelfResponse>> GetBookShelfById(Guid id);
+        Task<BaseResponse<BookShelfResponse>> UpdateBookShelf(Guid id, BookShelfRequest bookShelfRequest);
+        Task<BaseResponse<BookShelfResponse>> DeleteBookShelf(Guid id);
+    }
+}
