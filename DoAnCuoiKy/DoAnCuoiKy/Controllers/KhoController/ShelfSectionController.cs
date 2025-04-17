5d7ae032c181bdd7ea5aa25b209c0bbe3e127f83
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 19:39:51 +0700

    Create ShelfSectionController

---
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/KhoController/ShelfSectionController.cs
@@ -0,0 +1,26 @@
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+using DoAnCuoiKy.Service.IService.InformationLibrary.Kho;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKy.Controllers.KhoController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShelfSectionController : ControllerBase
+    {
+        private readonly IShelfSectionService _shelfSectionService;
+        public ShelfSectionController(IShelfSectionService shelfSectionService)
+        {
+            _shelfSectionService = shelfSectionService;
+        }
+
+        [HttpPost("AddShelfSection")]   
+        public async Task<BaseResponse<ShelfSectionResponse>> addShelfSection(ShelfSectionRequest shelfSectionRequest)
+        {
+            BaseResponse<ShelfSectionResponse> response = await _shelfSectionService.AddShelfSection(shelfSectionRequest);
+            return response;
+        }
+    }
+}
