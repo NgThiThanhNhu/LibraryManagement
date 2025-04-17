e8b17ffa9d9ccc05a03a4e296be4fb68414d8999
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:24:19 +0700

    add BookShelfRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/KhoRequest/BookShelfRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DoAnCuoiKy.Model.Request.KhoRequest
+{
+    public class BookShelfRequest
+    {
+        public string? BookShelfName { get; set; }
+        public int? NumberOfShelves { get; set; } // số kệ trong 1 tủ
+    }
+}
