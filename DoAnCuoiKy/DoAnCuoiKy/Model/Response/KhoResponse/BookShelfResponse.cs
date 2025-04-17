4bca1da17109d56ff30c3b548989aaab0f96bf84
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:24:58 +0700

    add BookShelfResponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/KhoResponse/BookShelfResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Response.KhoResponse
+{
+    public class BookShelfResponse
+    {
+        public Guid? Id { get; set; }
+        public string? BookShelfName { get; set; }
+        public int? NumberOfShelves { get; set; } // số kệ trong 1 tủ
+    }
+}
