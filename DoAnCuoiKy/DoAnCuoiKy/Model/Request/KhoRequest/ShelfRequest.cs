cc5b568d0623b357e2489840751c8828ce745012
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:24:35 +0700

    add ShelfRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/KhoRequest/ShelfRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Request.KhoRequest
+{
+    public class ShelfRequest
+    {
+        public string? ShelfName { get; set; }
+        public int? NumberOfSections { get; set; }
+        public Guid? BookshelfId { get; set; }  // Liên kết đến Tủ sách chứa Kệ sách này
+    }
+}
