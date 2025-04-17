de72d19213af5e423eff069801429c1a2dc11540
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 19:40:14 +0700

    Create ShelfSectionRequest

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Request/KhoRequest/ShelfSectionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Request.KhoRequest
+{
+    public class ShelfSectionRequest
+    {
+        public string? SectionName { get; set; }
+        public int? Capacity { get; set; } //số lượng sách có thể chứa của 1 ô sách
+        public Guid? ShelfId { get; set; }
+    }
+}
