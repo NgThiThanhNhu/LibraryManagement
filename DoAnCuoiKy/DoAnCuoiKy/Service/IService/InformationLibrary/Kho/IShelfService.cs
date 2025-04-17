56c0b4d02c2f915b5bf39e68bc3bfce1268888ab
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-17 18:27:22 +0700

    Create IShelfService

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IShelfService.cs
@@ -0,0 +1,15 @@
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+using DoAnCuoiKy.Model.Response;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
+{
+    public interface IShelfService
+    {
+        Task<BaseResponse<ShelfResponse>> AddShelf(ShelfRequest shelfRequest);
+        Task<BaseResponse<List<ShelfResponse>>> GetAllShelf();
+        Task<BaseResponse<ShelfResponse>> GetShelfById(Guid id);
+        Task<BaseResponse<ShelfResponse>> UpdateShelf(Guid id, ShelfRequest shelfRequest);
+        Task<BaseResponse<ShelfResponse>> DeleteShelf(Guid id);
+    }
+}
