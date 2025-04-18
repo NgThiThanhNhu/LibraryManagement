432663d1501ba488599a83739ad81f49b65d2b65
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-18 23:43:31 +0700

    create floorConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/FloorConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class FloorConfiguration : IEntityTypeConfiguration<Floor>
+    {
+        public void Configure(EntityTypeBuilder<Floor> builder)
+        {
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.FloorName)
+                .HasMaxLength(125);
+            
+        }
+    }
+}
