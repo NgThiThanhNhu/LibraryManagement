3d7c51255463149b21bbe1ca2ef18139bf41e252
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-18 23:44:55 +0700

    Create IRoomService

--- DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IRoomService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IRoomService.cs
@@ -1,7 +1,15 @@
-﻿namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
 {
     public interface IRoomService
     {
-        Task
+        Task<BaseResponse<RoomResponse>> AddRoom(RoomRequest roomRequest);
+        Task<BaseResponse<RoomResponse>> GetAllRoom();
+        Task<BaseResponse<RoomResponse>> GetRoomById(Guid id);
+        Task<BaseResponse<RoomResponse>> UpdateRoom(Guid id, RoomRequest roomRequest);
+        Task<BaseResponse<RoomResponse>> DeleteRoom(Guid id);
     }
 }
