6354ec368f61d6e150e268d529cfc2cfe61b85f1
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-18 20:26:40 +0700

    add ShelfSectionController

--- DoAnCuoiKy/DoAnCuoiKy/Controllers/KhoController/ShelfSectionController.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/KhoController/ShelfSectionController.cs
@@ -22,5 +22,33 @@ namespace DoAnCuoiKy.Controllers.KhoController
             BaseResponse<ShelfSectionResponse> response = await _shelfSectionService.AddShelfSection(shelfSectionRequest);
             return response;
         }
+
+        [HttpGet("GetAllShelfSection")]
+        public async Task<BaseResponse<List<ShelfSectionResponse>>> getAllShelfSection()
+        {
+            BaseResponse<List<ShelfSectionResponse>> response = await _shelfSectionService.GetAllShelfSection();
+            return response;
+        }
+
+        [HttpGet("GetShelfSectionById/{id}")]
+        public async Task<BaseResponse<ShelfSectionResponse>> getShelfSectionById(Guid id)
+        {
+            BaseResponse<ShelfSectionResponse> response = await _shelfSectionService.GetShelfSectionById(id);
+            return response;
+        }
+
+        [HttpPost("UpdateShelfSection/{id}")]
+        public async Task<BaseResponse<ShelfSectionResponse>> updateShelfSection(Guid id, ShelfSectionRequest shelfSectionRequest)
+        {
+            BaseResponse<ShelfSectionResponse> response = await _shelfSectionService.UpdateShelfSection(id, shelfSectionRequest);
+            return response;
+        }
+
+        [HttpPost("DeleteShelfSection/{id}")]
+        public async Task<BaseResponse<ShelfSectionResponse>> deleteShelfSection(Guid id)
+        {
+            BaseResponse<ShelfSectionResponse> response = await _shelfSectionService.DeleteShelfSection(id);
+            return response;
+        }
     }
 }
