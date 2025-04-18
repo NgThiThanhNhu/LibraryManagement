d810dc006d80700119207b1f25d0994f57437556
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-18 22:01:18 +0700

    update IFloorService

--- DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IFloorService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/Kho/IFloorService.cs
@@ -1,6 +1,15 @@
-﻿namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary.Kho
 {
     public interface IFloorService
     {
+        Task<BaseResponse<FloorResponse>> AddFloor(FloorRequest floorRequest);
+        Task<BaseResponse<List<FloorResponse>>> GetAllFloor();
+        Task<BaseResponse<FloorResponse>> GetFloorById(Guid id);
+        Task<BaseResponse<FloorResponse>> UpdateFloor(Guid id, FloorRequest floorRequest);
+        Task<BaseResponse<FloorResponse>> DeleteFloor(Guid id);
     }
 }
