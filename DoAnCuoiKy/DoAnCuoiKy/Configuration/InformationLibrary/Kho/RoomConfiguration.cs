8abf2896de05233a05eb7254d6c8730aebe3b555
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-18 23:43:53 +0700

    create RoomConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/RoomConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
+{
+    public class RoomConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.HasKey(room => room.Id);
+            builder.Property(room => room.RoomName)
+                .HasMaxLength(125);
+            //mối quan hệ với Floor
+            builder.HasOne(room => room.Floor)
+                .WithMany(f => f.Rooms)
+                .HasForeignKey(room => room.FloorId);
+        }
+    }
+}
