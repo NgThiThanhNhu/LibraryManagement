3260aeffc0681cee5dfe6291481c2a35c1fb0f24
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-20 21:43:14 +0700

    create Roomresponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/KhoResponse/RoomResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Response.KhoResponse
+{
+    public class RoomResponse
+    {
+        public Guid? Id { get; set; }
+        public string? RoomName { get; set; }
+        public string? FloorName { get; set; }
+    }
+}
