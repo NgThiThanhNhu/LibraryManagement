38d3908fd55384ea8096ca6dbe3882ad75ec2eba
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-21 17:31:05 +0700

    update LocationResponse

--- DoAnCuoiKy/DoAnCuoiKy/Model/Response/KhoResponse/LocationResponse.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/KhoResponse/LocationResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace DoAnCuoiKy.Model.Response.KhoResponse
+﻿using DoAnCuoiKy.Model.Enum.InformationLibrary.Kho;
+
+namespace DoAnCuoiKy.Model.Response.KhoResponse
 {
     public class LocationResponse
     {
@@ -7,5 +9,8 @@
         public string? Description { get; set; }
         public string? Room { get; set; }
         public string? Floor { get; set; }
+        public string? ShelfName { get; set; }
+        public string? BookShelfName {  get; set; }
+        public LocationStatus? LocationStatus { get; set; }
     }
 }
