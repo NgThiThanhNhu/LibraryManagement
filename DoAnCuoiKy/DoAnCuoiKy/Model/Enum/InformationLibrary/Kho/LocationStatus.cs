44595aa1d1d2f304435792e5019fa6ac858c3c52
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-21 17:30:43 +0700

    create LocationStatus

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/Kho/LocationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary.Kho
+{
+    public enum LocationStatus : byte
+    {
+        Empty = 0, //chưa có quyển nào
+        Available = 1, //còn chỗ
+        Full = 2 //đã đầy
+
+    }
+}
