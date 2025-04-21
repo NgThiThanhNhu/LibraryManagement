1e54c6a63e6742ebafa55da0cd58a096bf90c0b2
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-21 17:33:49 +0700

    create LocationController add AddLocation

---
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/KhoController/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using DoAnCuoiKy.Model.Request.KhoRequest;
+using DoAnCuoiKy.Model.Response.KhoResponse;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Service.IService.InformationLibrary.Kho;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKy.Controllers.KhoController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+        public LocationController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+        [HttpPost("AddLocation")]
+        public async Task<BaseResponse<LocationResponse>> addLocation(LocationRequest locationRequest)
+        {
+            BaseResponse<LocationResponse> response = await _locationService.AddLocation(locationRequest);
+            return response;
+        }
+    }
+}
