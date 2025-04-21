ab8180d301a8bb6aff818807bc28dc698fe4b18e
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-21 17:31:20 +0700

    update Location

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Location.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Kho/Location.cs
@@ -1,6 +1,8 @@
-﻿namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
+﻿using DoAnCuoiKy.Model.Enum.InformationLibrary.Kho;
+
+namespace DoAnCuoiKy.Model.Entities.InformationLibrary.Kho
 {
-    public class Location : LocationBase
+    public class Location : BaseEntity
     {
         public Guid? Id { get; set; }
         public Guid? ShelfSectionId {  get; set; }
@@ -8,6 +10,10 @@
         public List<BookItem>? BookItems { get; set; } //1 vị trí là địa chỉ củ 1 cuốn sách
         //nhưng 1 địa chỉ đó có chứa nhiều cuốn sách ví dụ: ở entity shelfsection có thuộc tính Capacity nghĩa là sức chứa của 1 ô section
         public string? Description { get; set; }
+        //khi thêm bookitem thì location của kho phải có trước, và biết tình trạng của location đó còn trống hay không
+        //khi thêm bookitem thì chọn vị trí để sách vào 
+        public LocationStatus? LocationStatus { get; set; } = 0;
+
 
     }
 }
