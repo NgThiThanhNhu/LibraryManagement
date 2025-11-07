dbedfa99c4ba7dd3ec2fa16badf1e148b0cd8731
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-11-07 14:00:58 +0700

    ThanhNhu: update property BorrowingDetail

--- DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/BorrowingDetailConfiguration.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/BorrowingDetailConfiguration.cs
@@ -9,21 +9,15 @@ namespace DoAnCuoiKy.Configuration.InformationLibrary
         public void Configure(EntityTypeBuilder<BorrowingDetail> builder)
         {
             builder.HasKey(bod => bod.Id);
-
-           
-
+            builder.Property(bod => bod.IsFined);
             //với bảng borrowing
             builder.HasOne(bod => bod.borrowing)
                 .WithMany(bo => bo.borrowingDetails)
                 .HasForeignKey(bod => bod.BorrowingId);
-
-            
             //với bảng BookItem
             builder.HasOne(bod=>bod.bookItem)
                 .WithMany(i=>i.borrowingDetails)
                 .HasForeignKey(bod=>bod.BookItemId);
-
-
         }
     }
 }
