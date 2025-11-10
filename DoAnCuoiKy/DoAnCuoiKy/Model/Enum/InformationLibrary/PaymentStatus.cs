70a7b7b2d0d67d188b0245e2b575b354cfffb5e7
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-11-10 17:16:40 +0700

    ThanhNhu: add paymenttype and paymentstatus

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary
+{
+    public enum PaymentStatus : byte
+    {
+        Pending = 0,
+        Success = 1,
+        Failed = 2
+    }
+}
---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/PaymentType.cs
@@ -0,0 +1,9 @@
+﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary
+{
+    public enum PaymentType : byte
+    {
+        Cash = 1,
+        VNPay = 2,
+        Momo = 3
+    }
+}
