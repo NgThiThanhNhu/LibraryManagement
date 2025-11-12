c9624691b5e955e567671d2cda81e3c67cfd3843
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-11-12 23:25:18 +0700

    ThanhNhu: update kieu du lieu

--- DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookImportTransactionConfiguration.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/Kho/BookImportTransactionConfiguration.cs
@@ -9,12 +9,13 @@ namespace DoAnCuoiKy.Configuration.InformationLibrary.Kho
         public void Configure(EntityTypeBuilder<BookImportTransaction> builder)
         {
             builder.HasKey(ip => ip.Id);
-
-            //builder.Property(ip => ip.Quantity);
-
-            //builder.Property(ip => ip.UnitPrice);
-
-            //builder.Property(ip => ip.TotalPrice);
+            builder.Property(ip => ip.Quantity);
+            builder.Property(ip => ip.UnitPrice)
+                .HasColumnType("decimal(18,2)");
+            builder.Property(ip => ip.TotalPrice)
+                .HasColumnType("decimal(18,2)");
+            builder.Property(ip => ip.TransactionType)
+                .HasConversion<byte>();
         }
     }
 }
