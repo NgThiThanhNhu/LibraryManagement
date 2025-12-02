4862eaedda5985a5b95d53c1d61cc8eb2e7a932b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:38:56 +0700

    ThanhNhu: add bookcart

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/BookCart.cs
@@ -0,0 +1,15 @@
+﻿using DoAnCuoiKy.Model.Entities.Usermanage;
+using DoAnCuoiKy.Model.Enum.InformationLibrary;
+
+namespace DoAnCuoiKy.Model.Entities.InformationLibrary
+{
+    public class BookCart : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Librarian Librarian { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public CartStatus CartStatus { get; set; }
+        public List<BookCartItem>? BookCartItems { get; set; }
+    }
+}
