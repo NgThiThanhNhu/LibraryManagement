747348ec021bd61e0adb3fddb8b4a13b0a7440c0
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:32:29 +0700

    ThanhNhu: add BookCartConfiguration and PaymentConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/BookCartConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary
+{
+    public class BookCartConfiguration : IEntityTypeConfiguration<BookCart>
+    {
+        public void Configure(EntityTypeBuilder<BookCart> builder)
+        {
+            builder.HasKey(bc => bc.Id);
+            builder.HasOne(bc => bc.Librarian)
+                .WithMany(u => u.bookCarts)
+                .HasForeignKey(bc => bc.UserId);
+            builder.Property(bc => bc.CartStatus)
+               .HasConversion<byte>()
+               .HasMaxLength(20);
+            builder.HasIndex(x => new { x.UserId, x.CartStatus })
+                .HasDatabaseName("IX_BookCarts_UserId_Status");
+        }
+    }
+}
---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/InformationLibrary/PaymentConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.InformationLibrary
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.PaymentType)
+                .HasConversion<byte>();
+            builder.Property(x => x.TransactionNo);
+            builder.Property(x => x.VnpText);
+            builder.Property(x=>x.VnpResponseCode);
+            builder.HasOne(x => x.Borrowing)
+                .WithOne(y => y.Payment)
+                .HasForeignKey<Payment>(x => x.BorrowingId);
+            builder.Property(x => x.BorrowAmount)
+                .HasColumnType("decimal(18,2)");
+            builder.Property(x => x.OrderId)
+                .HasMaxLength(50);
+        }
+    }
+}
