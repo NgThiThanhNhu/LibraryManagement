3757b3dd52740c640d4a69c135ff8e5e0277c0bb
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:43:55 +0700

    thanhnhu: add entity user search history

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/UserBehavior/UserSearchHistory.cs
@@ -0,0 +1,17 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+
+namespace DoAnCuoiKy.Model.Entities.UserBehavior
+{
+    public class UserSearchHistory : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid? UserId { get; set; }
+        public string SearchKeyword { get; set; }
+        public string SearchType { get; set; }
+        public int ResultCount { get; set; }
+        public List<Book> Books { get; set; }
+        public string IpAddress { get; set; }
+        public Librarian Librarian { get; set; }
+    }
+}
