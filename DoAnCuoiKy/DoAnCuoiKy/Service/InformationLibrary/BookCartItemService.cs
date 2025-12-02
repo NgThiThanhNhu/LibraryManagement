31fded3ae8fd585c7f73f5ad8ededbcc9b0e2273
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:45:38 +0700

    thanhnhu: update cart item service

--- DoAnCuoiKy/DoAnCuoiKy/Service/InformationLibrary/BookCartItemService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/InformationLibrary/BookCartItemService.cs
@@ -2,7 +2,7 @@
 using System.Security.Claims;
 using DoAnCuoiKy.Data;
 using DoAnCuoiKy.Model.Entities.InformationLibrary;
-using DoAnCuoiKy.Model.Entities.Usermanage;
+using DoAnCuoiKy.Model.Enum.InformationLibrary;
 using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 using DoAnCuoiKy.Service.IService.InformationLibrary;
@@ -13,86 +13,128 @@ namespace DoAnCuoiKy.Service.InformationLibrary
     public class BookCartItemService : IBookCartItemService
     {
         private readonly ApplicationDbContext _context;
-        private readonly IHttpContextAccessor _contextAccessor;
-        public BookCartItemService(ApplicationDbContext context, IHttpContextAccessor contextAccessor)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public BookCartItemService(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
-            _contextAccessor = contextAccessor;
-        }
-       
-
-        public async Task<BaseResponse<BookCartItemResponse>> AddBookItemToCart(BookCartItemRequest bookCartItemRequest)
-        {
-            BaseResponse<BookCartItemResponse> response = new BaseResponse<BookCartItemResponse>();
-            BookCartItem bookCartItem = new BookCartItem();
-            bookCartItem.Id = Guid.NewGuid();
-            bookCartItem.UserId = getCurrentUserId();
-            Librarian librarian = await _context.librarians.Where(x=>x.isValidate == true && x.Id == bookCartItem.UserId).FirstOrDefaultAsync();
-            bookCartItem.BookItemId = bookCartItemRequest.BookItemId;
-            bookCartItem.CreateDate = DateTime.Now;
-            bookCartItem.CreateUser = librarian.Name;
-            _context.bookCartItems.Add(bookCartItem);
-            await _context.SaveChangesAsync();
-            BookItem bookItem = await _context.bookItems.Include(x=>x.Book).ThenInclude(x=>x.BookAuthor).Include(x=>x.Book).ThenInclude(x=>x.Category).Where(x => x.IsDeleted == false && x.Id == bookCartItem.BookItemId).FirstOrDefaultAsync();
-            BookCartItemResponse bookCartItemResponse = new BookCartItemResponse();
-            bookCartItemResponse.Id = bookCartItem.Id;
-            bookCartItemResponse.BookItemTitle = bookItem.Book.Title;
-            bookCartItemResponse.BookItemAuthor = bookItem.Book.BookAuthor.Name;
-            bookCartItemResponse.BookItemCategory = bookItem.Book.Category.Name;
-            bookCartItemResponse.UserName = librarian.Name;
-            response.IsSuccess = true;
-            response.message = "Thêm sách vào giỏ hàng thành công";
-            response.data = bookCartItemResponse;
-            return response;
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        public async Task<BaseResponse<BookCartItemResponse>> DeleteBookCartItem(Guid currentBookItemId)
+        public async Task<BaseResponse<bool>> RemoveItem(Guid bookCartItemId)
         {
-            BaseResponse<BookCartItemResponse> response = new BaseResponse<BookCartItemResponse>();
-            var currentUser = getCurrentUserId();
-            BookCartItem bookCartItem = await _context.bookCartItems.Include(x=>x.BookItem).Where(x => x.IsDeleted == false && x.UserId == currentUser && x.BookItemId == currentBookItemId).FirstOrDefaultAsync();
-            if(bookCartItem.BookItem.BookStatus == Model.Enum.InformationLibrary.BookStatus.Borrowed && bookCartItem != null)
+            BaseResponse<bool> response = new BaseResponse<bool>();
+            try
             {
-                bookCartItem.IsDeleted = true;
-
-                _context.bookCartItems.Update(bookCartItem);
+                var currentUserId = getCurrentUserId();
+                BookCartItem findBookCartItem = await _context.bookCartItems.Include(x => x.BookCart).Include(x=>x.Book).FirstOrDefaultAsync(x => x.IsDeleted == false && x.Id == bookCartItemId);
+                if (findBookCartItem == null || findBookCartItem.BookCart.UserId != currentUserId)
+                {
+                    response.IsSuccess = false;
+                    response.message = "Không tìm thấy sách";
+                    response.data = false;
+                    return response;
+                }
+                var bookTitle = findBookCartItem.Book.Title;
+                _context.bookCartItems.Remove(findBookCartItem);
+                findBookCartItem.BookCart.UpdateDate = DateTime.Now;
+                findBookCartItem.BookCart.UpdateUser = getCurrentName();
+                _context.bookCarts.Update(findBookCartItem.BookCart);
                 await _context.SaveChangesAsync();
-                
                 response.IsSuccess = true;
-                response.message = "Đã đặt sách thành công";
+                response.message = $"Xóa sách {bookTitle} khỏi giỏ thành công";
+                response.data = true;
                 return response;
-            }
-            else
+            }catch (Exception ex)
             {
-                response.IsSuccess = false;
-                response.message = "Không tìm thấy sách cần xóa của user này";
+                response.IsSuccess= false;
+                response.message = ex.Message;
+                response.data = false;
                 return response;
             }
         }
 
-        public async Task<BaseResponse<List<BookCartItemResponse>>> GetAllBookCartOfUser()
+        public async Task<BaseResponse<int>> UpdateQuantity(Guid bookCartItemId, UpdateQuantityRequest updateQuantityRequest)
         {
-            BaseResponse<List<BookCartItemResponse>> response = new BaseResponse<List<BookCartItemResponse>>();
-            var currentUserId = getCurrentUserId();
-            List<BookCartItemResponse> bookCartItems = await _context.bookCartItems.Include(x => x.BookItem.Book).ThenInclude(x => x.BookAuthor).Include(x => x.BookItem.Book).ThenInclude(x => x.Category).Include(x => x.BookItem.Book).ThenInclude(x => x.bookFiles).Where(x => x.UserId == currentUserId && x.IsDeleted == false).Select(bookcartitem => new BookCartItemResponse {
-                Id = bookcartitem.Id,
-                BookItemId = bookcartitem.BookItemId,
-                BookItemTitle = bookcartitem.BookItem.Book.Title,
-                BookItemAuthor = bookcartitem.BookItem.Book.BookAuthor.Name,
-                BookItemCategory = bookcartitem.BookItem.Book.Category.Name,
-                UserName = bookcartitem.User.Name,
-                ImageUrls = bookcartitem.BookItem.Book.bookFiles.Where(y => y.BookId == bookcartitem.BookItem.Book.Id).Select(y => y.ImageUrl).ToList()
-            }).ToListAsync();
-            response.IsSuccess = true;
-            response.message = "Đây là tất cả sách hiện có trong giỏ hàng của bạn";
-            response.data = bookCartItems;
-            return response;
-
+            BaseResponse<int> response = new BaseResponse<int>();
+            try
+            {
+                var currentUserId = getCurrentUserId();
+                BookCartItem findCartItem = await _context.bookCartItems.Include(x => x.BookCart).Include(x => x.Book).FirstOrDefaultAsync(x => x.Id == bookCartItemId && x.IsDeleted == false && x.BookCart.UserId == currentUserId);
+                if (findCartItem == null)
+                {
+                    response.IsSuccess = false;
+                    response.message = "Không tìm thấy sách trong giỏ";
+                    return response;
+                }
+                int newQuantity = findCartItem.Quantity;
+                if (updateQuantityRequest.Action == "increase")
+                {
+                    newQuantity++;
+                }
+                else if (updateQuantityRequest.Action == "decrease")
+                {
+                    newQuantity--;
+                }
+                else
+                {
+                    response.IsSuccess = false;
+                    response.message = "Action không hợp lệ";
+                    return response;
+                }
+                if (newQuantity < 1)
+                {
+                    response.IsSuccess = false;
+                    response.message = "Số lượng không được nhỏ hơn 1";
+                    return response;
+                }
+                if (newQuantity > 5)
+                {
+                    response.IsSuccess = false;
+                    response.message = "Không mượn quá 5 cuốn";
+                    return response;
+                }
+                int checkOrderBookCartItem = await _context.bookCartItems.Where(x => x.CartId == findCartItem.CartId && x.Id != findCartItem.Id && x.IsDeleted == false).SumAsync(x => x.Quantity);
+                if (checkOrderBookCartItem + newQuantity > 5)
+                {
+                    response.IsSuccess = false;
+                    response.message = "Số lượng sách trong giỏ không được nhiều hơn 5 cuốn";
+                    return response;
+                }
+                if (newQuantity > findCartItem.Quantity)
+                {
+                    int availableBookItemCount = await _context.bookItems.CountAsync(x => x.IsDeleted == false && x.BookId == findCartItem.BookId && x.BookStatus == Model.Enum.InformationLibrary.BookStatus.Available);
+                    if (availableBookItemCount < newQuantity)
+                    {
+                        response.IsSuccess = false;
+                        response.message = "Không đủ sách";
+                        return response;
+                    }
+                }
+                
+                findCartItem.Quantity = newQuantity;
+                findCartItem.UpdateDate = DateTime.Now;
+                findCartItem.UpdateUser = getCurrentName();
+                _context.bookCartItems.Update(findCartItem);
+                await _context.SaveChangesAsync();
+                response.IsSuccess = true;
+                response.message = "Thêm sách thành công";
+                response.data = newQuantity;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess=false;
+                response.message = ex.Message;
+                return response;
+            }
         }
-
+        private string getCurrentName()
+        {
+            return _httpContextAccessor.HttpContext.User.Identity.Name;
+        }
         private Guid getCurrentUserId()
         {
-            var user = _contextAccessor.HttpContext.User;
+            var user = _httpContextAccessor.HttpContext.User;
             if (user == null || !user.Identity.IsAuthenticated)
             {
                 throw new UnauthorizedAccessException("User is not Authentiated");
@@ -103,6 +145,37 @@ namespace DoAnCuoiKy.Service.InformationLibrary
                 throw new Exception("userId không tồn tại");
             }
             return Guid.Parse(userId.Value);
+        }
+        public async Task<BaseResponse<bool>> ClearAllBookCartItem(Guid bookCartId)
+        {
+            BaseResponse<bool> response = new BaseResponse<bool>();
+            try
+            {
+                var currentUserId = getCurrentUserId();
+                BookCart findBookCart = await _context.bookCarts.Include(x => x.BookCartItems).FirstOrDefaultAsync(x => x.IsDeleted == false && x.UserId == currentUserId && x.Id == bookCartId && x.CartStatus == CartStatus.Active);
+                if (findBookCart == null || !findBookCart.BookCartItems.Any())
+                {
+                    response.IsSuccess = false;
+                    response.message = "Không tìm thấy giỏ sách muốn xóa";
+                    return response;
+                }
+                findBookCart.BookCartItems.Clear();
+                findBookCart.UpdateDate = DateTime.Now;
+                findBookCart.UpdateUser = getCurrentName();
+                _context.bookCarts.Update(findBookCart);
+                await _context.SaveChangesAsync();
+                response.IsSuccess = true;
+                response.message = "Xóa giỏ sách thành công";
+                response.data = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.message = ex.Message;
+                response.data = false;
+                return response;
+            }
         }
     }
 }
