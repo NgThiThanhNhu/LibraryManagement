a5d97f8065a0615c2942249f3ada306a3b86ef2d
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:46:58 +0700

    thanhnhu: add interface payment service

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using DoAnCuoiKy.Model.Request;
+using DoAnCuoiKy.Model.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary
+{
+    public interface IPaymentService
+    {
+        Task<BaseResponse<PaymentResponse>> CreatePayment(PaymentRequest paymentRequest);
+        Task<BaseResponse<PaymentResponse>> VnPayReturn(IQueryCollection query);
+        Task<BaseResponse<PaymentResponse>> CashPayment(PaymentRequest paymentRequest);
+    }
+}
