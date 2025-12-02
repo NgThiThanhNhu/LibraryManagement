0853d253da53ee1843c0d2754189eeb89566774b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:38:41 +0700

    ThanhNhu: update entity book about user behavior

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Book.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Book.cs
@@ -1,4 +1,5 @@
 ﻿using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
+using DoAnCuoiKy.Model.Entities.UserBehavior;
 using DoAnCuoiKy.Model.Entities.Usermanage;
 using DoAnCuoiKy.Model.Enum.InformationLibrary;
 using System.ComponentModel.DataAnnotations;
@@ -12,7 +13,7 @@ namespace DoAnCuoiKy.Model.Entities.InformationLibrary
         public Guid? PublisherId { get; set; }
         public Publisher? Publisher { get; set; }
         public Guid? BookAuthorId { get; set; }
-        public BookAuthor? BookAuthor { get; set; }
+        public BookAuthor BookAuthor { get; set; }
         public int? YearPublished { get; set; }
         public int Quantity { get; set; } //số lượng sách nhập
         public decimal? TotalPrice { get; set; }
@@ -26,5 +27,9 @@ namespace DoAnCuoiKy.Model.Entities.InformationLibrary
         public List<BookFile> bookFiles { get; set; }
         public string Description { get; set; }
         public string Slug { get; set; }
+        public List<BookCartItem> BookCartItems { get; set; }
+        public List<BookRecommendation> bookRecommendations { get; set; }
+        public List<UserBookInteraction> userBookInteractions { get; set; }
+        public List<UserReadingSession> userReadingSessions { get; set; }
     }
 }
