763af278c474166a33558afedbb9b652142a141d
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:44:16 +0700

    thanhnhu: update entity librarian

--- DoAnCuoiKy/DoAnCuoiKy/Model/Entities/Usermanage/Librarian.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/Usermanage/Librarian.cs
@@ -1,6 +1,7 @@
 ﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
 using DoAnCuoiKy.Model.Entities.InformationLibrary.Kho;
 using DoAnCuoiKy.Model.Entities.Notification;
+using DoAnCuoiKy.Model.Entities.UserBehavior;
 using System.Runtime.CompilerServices;
 
 namespace DoAnCuoiKy.Model.Entities.Usermanage
@@ -15,12 +16,16 @@ namespace DoAnCuoiKy.Model.Entities.Usermanage
         public string? Salt { get; set; }
         public Guid? RoleId { get; set; } //phân quyền
         public Role? Role { get; set; }
-       
         //1librarian-nhiều transaction
         public bool isValidate { get; set; } = false;
         public List<Borrowing> borrowings { get; set; }
         public List<Fine>? fines { get; set; }
-        public List<BookCartItem> bookCartItems { get; set; }
+        public List<BookCart> bookCarts { get; set; }
         public List<NotificationToUser> notifications { get; set; }
+        public List<BookRecommendation> bookRecommendations { get; set; }
+        public List<UserReadingSession> userReadingSessions { get; set; }
+        public List<UserBookInteraction> userBookInteractions { get; set; }
+        public List<UserCategoryPreference> categoryPreference { get; set; }
+        public List<UserSearchHistory> userSearchHistories { get; set; }
     }
 }
