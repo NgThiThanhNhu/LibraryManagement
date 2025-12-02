6ed60d56cdfddaa93f127a6e213e03a777e6a269
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:36:35 +0700

    ThanhNhu: add userreadingSessionConfiguration

---
+++ DoAnCuoiKy/DoAnCuoiKy/Configuration/UserBehavior/UserReadingSessionConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DoAnCuoiKy.Model.Entities.UserBehavior;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoAnCuoiKy.Configuration.UserBehavior
+{
+    public class UserReadingSessionConfiguration : IEntityTypeConfiguration<UserReadingSession>
+    {
+        public void Configure(EntityTypeBuilder<UserReadingSession> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Librarian)
+                .WithMany(l => l.userReadingSessions)
+                .HasForeignKey(x => x.UserId);
+            builder.HasOne(x=>x.Book)
+                .WithMany(b=>b.userReadingSessions)
+                .HasForeignKey(x => x.BookId);
+            builder.Property(e => e.DeviceType)
+                .HasMaxLength(20);
+            builder.Property(x => x.ReadingProgress)
+                .HasPrecision(5, 2);
+            builder.HasIndex(x => x.UserId)
+                .HasDatabaseName("IX_UserReadingSession_UserId");
+            builder.HasIndex(x => x.BookId)
+                .HasDatabaseName("IX_UserReadingSession_BookId");
+            builder.HasIndex(x => new { x.UserId, x.BookId, x.StartTime })
+                .HasDatabaseName("IX_UserReadingSession_Composite");
+            builder.HasIndex(x => x.StartTime)
+                .HasDatabaseName("IX_UserReadingSession_StartTime");
+        }
+    }
+}
