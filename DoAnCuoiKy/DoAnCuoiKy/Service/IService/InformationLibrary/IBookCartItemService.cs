da000b8473eb13ab880b01ffc5fd63ff46f795e8
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:46:32 +0700

    thanhnhu: add interface cart service and cart item service

--- DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookCartItemService.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookCartItemService.cs
@@ -1,13 +1,13 @@
-﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
-using DoAnCuoiKy.Model.Request;
+﻿using DoAnCuoiKy.Model.Request;
 using DoAnCuoiKy.Model.Response;
 
 namespace DoAnCuoiKy.Service.IService.InformationLibrary
 {
     public interface IBookCartItemService
     {
-        Task<BaseResponse<BookCartItemResponse>> AddBookItemToCart(BookCartItemRequest bookCartItemRequest);
-        Task<BaseResponse<List<BookCartItemResponse>>> GetAllBookCartOfUser();
-        Task<BaseResponse<BookCartItemResponse>> DeleteBookCartItem(Guid currentBookItemid);
+        Task<BaseResponse<int>> UpdateQuantity(Guid bookCartItemId, UpdateQuantityRequest updateQuantityRequest);
+        Task<BaseResponse<bool>> RemoveItem(Guid bookCartItemId);
+        Task<BaseResponse<bool>> ClearAllBookCartItem(Guid bookCartId);
+
     }
 }
---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/InformationLibrary/IBookCartService.cs
@@ -0,0 +1,13 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Request;
+using DoAnCuoiKy.Model.Response;
+
+namespace DoAnCuoiKy.Service.IService.InformationLibrary
+{
+    public interface IBookCartService
+    {
+        Task<BaseResponse<BookCartResponse>> CreateBookCart(BookCartRequest bookCartRequest);
+        Task<BaseResponse<BookCartResponse>> GetBookCartActive();
+        Task<BaseResponse<CheckoutBookCartResponse>> CheckoutBookCart(CheckoutBookCartResquest checkoutBookCartResquest);
+    }
+}
