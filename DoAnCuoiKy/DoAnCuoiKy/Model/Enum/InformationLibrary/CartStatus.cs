53744b2203b9391f3dd6923e7a6dae66cf7d28a5
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:44:53 +0700

    thanhnhu: add enum cartstatus

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/CartStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary
+{
+    public enum CartStatus : byte
+    {
+        Active = 1,
+        Saved = 2,
+        Expired = 3,
+        CheckedOut = 4,
+        Cancelled = 5
+    }
+}
--- DoAnCuoiKy/DoAnCuoiKy/Model/Enum/InformationLibrary/PaymentStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DoAnCuoiKy.Model.Enum.InformationLibrary
-{
-    public enum PaymentStatus : byte
-    {
-        Pending = 0,
-        Success = 1,
-        Failed = 2
-    }
-}
