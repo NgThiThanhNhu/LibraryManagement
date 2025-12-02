e753a3eb9087cbf7f7587bc3baabfdb6da12e8f9
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:43:15 +0700

    ThanhNhu: add entity user category preference

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/UserBehavior/UserCategoryPreference.cs
@@ -0,0 +1,19 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+
+namespace DoAnCuoiKy.Model.Entities.UserBehavior
+{
+    public class UserCategoryPreference : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CategoryId { get; set; }
+        public decimal PreferenceScore { get; set; }
+        public int ViewCount { get; set; }
+        public int BorrowCount { get; set; }
+        public int ReadingMinutes { get; set; }
+        public DateTime LastInteraction {  get; set; }
+        public Librarian Librarian { get; set; }
+        public BookCategory BookCategory { get; set; }
+    }
+}
