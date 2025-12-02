7446455108baa8a197edbdadaeb460c2a9e0d4f3
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:43:37 +0700

    thanhnhu: add entity user reading session

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/UserBehavior/UserReadingSession.cs
@@ -0,0 +1,22 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+
+namespace DoAnCuoiKy.Model.Entities.UserBehavior
+{
+    public class UserReadingSession
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Librarian Librarian { get; set; }
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int DurationSeconds { get; set; }
+        public int LastPageNumber { get; set; }
+        public int TotalPages { get; set; }
+        public decimal ReadingProgress { get; set; }
+        public string DeviceType { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
