ae9b7fe9e455e3b5dcb66cbcd1c3938c41103390
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:42:52 +0700

    ThanhNhu: add entity user book interaction

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/UserBehavior/UserBookInteraction.cs
@@ -0,0 +1,20 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+using DoAnCuoiKy.Model.Enum.UserBehavior;
+
+namespace DoAnCuoiKy.Model.Entities.UserBehavior
+{
+    public class UserBookInteraction : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Librarian Librarian { get; set; }
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+        public InteractionType InteractionType { get; set; }
+        public DateTime InteractionDate { get; set; }
+        public int Rating {  get; set; }
+        public string ReviewText { get; set; }
+        public int DurationSeconds { get; set; }
+    }
+}
