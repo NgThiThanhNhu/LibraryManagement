6bf0ec7752dce77265e27c0a56bfc0c5106c1526
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-02 17:41:34 +0700

    ThanhNhu: add entity payment

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Entities/InformationLibrary/Payment.cs
@@ -0,0 +1,19 @@
+﻿using DoAnCuoiKy.Model.Enum.InformationLibrary;
+
+namespace DoAnCuoiKy.Model.Entities.InformationLibrary
+{
+    public class Payment : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string? OrderId { get; set; }           // Mã thanh toán VNPay
+        public string? VnpText { get; set; }
+        public string? VnpResponseCode { get; set; }
+        public PaymentType? PaymentType { get; set; } // VNPay, Cash...
+        public string? TransactionNo { get; set; }
+        public Guid? BorrowingId { get; set; }
+        public Borrowing? Borrowing { get; set; }
+        public decimal? BorrowAmount { get; set; }
+        public string? vnpBankCode { get; set; }
+    }
+
+}
