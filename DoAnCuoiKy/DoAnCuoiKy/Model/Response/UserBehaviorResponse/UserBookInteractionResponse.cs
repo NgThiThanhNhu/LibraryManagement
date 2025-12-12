ab3827dceb79ec2c4d0ab0e002a05d0dbc92c38b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-12 08:57:17 +0700

    ThanhNhu: Add BookInteractionResponse and readingSessionResponse

---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/UserBehaviorResponse/UserBookInteractionResponse.cs
@@ -0,0 +1,16 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+using DoAnCuoiKy.Model.Enum.UserBehavior;
+
+namespace DoAnCuoiKy.Model.Response.UserBehaviorRequest
+{
+    public class UserBookInteractionResponse
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string BookTitle { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+    }
+}
---
+++ DoAnCuoiKy/DoAnCuoiKy/Model/Response/UserBehaviorResponse/UserReadingSessionResponse.cs
@@ -0,0 +1,19 @@
+﻿using DoAnCuoiKy.Model.Entities.InformationLibrary;
+using DoAnCuoiKy.Model.Entities.Usermanage;
+
+namespace DoAnCuoiKy.Model.Response.UserBehaviorResponse
+{
+    public class UserReadingSessionResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int DurationSeconds { get; set; }
+        public int LastPageNumber { get; set; }
+        public int TotalPages { get; set; }
+        public decimal ReadingProgress { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
