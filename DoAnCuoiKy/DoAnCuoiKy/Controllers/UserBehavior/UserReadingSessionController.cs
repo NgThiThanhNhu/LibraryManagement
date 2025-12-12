3c52dcd33e84312f8c044c2c7fb2469a477acdf8
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-12 08:54:05 +0700

    ThanhNhu: add UserReadingSessionController

---
+++ DoAnCuoiKy/DoAnCuoiKy/Controllers/UserBehavior/UserReadingSessionController.cs
@@ -0,0 +1,37 @@
+﻿using DoAnCuoiKy.Model.Request.UserBookInteractionRequest;
+using DoAnCuoiKy.Model.Response.UserBehaviorResponse;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Service.IService.IUserBehavior;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKy.Controllers.UserBehavior
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserReadingSessionController : ControllerBase
+    {
+        private readonly IUserReadingSessionService _userReadingSessionService;
+        public UserReadingSessionController(IUserReadingSessionService userReadingSessionService)
+        {
+            _userReadingSessionService = userReadingSessionService;
+        }
+        [HttpPost("createReadingSession")]
+        public async Task<BaseResponse<UserReadingSessionResponse>> CreateReadingSession(UserReadingSessionRequest readingSessionRequest)
+        {
+            BaseResponse<UserReadingSessionResponse> baseResponse = await _userReadingSessionService.CreateReadingSession(readingSessionRequest);
+            return baseResponse;
+        }
+        [HttpPost("updateReadingSession/{sessionId}")]
+        public async Task<BaseResponse<UserReadingSessionResponse>> UpdateReadingSession(Guid sessionId, UserReadingSessionUpdateRequest updateRequest)
+        {
+            BaseResponse<UserReadingSessionResponse> baseResponse = await _userReadingSessionService.UpdateReadingSession(sessionId, updateRequest);
+            return baseResponse;
+        }
+        [HttpGet("book/{bookId}/last-position")]
+        public async Task<BaseResponse<UserReadingSessionResponse>> GetLastPosition(Guid bookId)
+        {
+            BaseResponse<UserReadingSessionResponse> baseResponse = await _userReadingSessionService.GetLastPosition(bookId);
+            return baseResponse;
+        }
+    }
+}
