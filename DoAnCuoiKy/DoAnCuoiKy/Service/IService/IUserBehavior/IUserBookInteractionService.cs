f35c958a97772ad55dccf8db764c60ea537c0a3e
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-12 08:59:05 +0700

    ThanhNhu: add IService for user behavior

---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/IUserBehavior/IUserBookInteractionService.cs
@@ -0,0 +1,12 @@
+﻿using DoAnCuoiKy.Model.Request.UserBookInteractionRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.UserBehaviorRequest;
+
+namespace DoAnCuoiKy.Service.IService.IUserBehavior
+{
+    public interface IUserBookInteractionService
+    {
+        Task<BaseResponse<UserBookInteractionResponse>> CreateRating(UserBookInteractionRequest userBookInteractionRequest);
+        Task<BaseResponse<List<UserBookInteractionResponse>>> GetAllRating(Guid BookId);
+    }
+}
---
+++ DoAnCuoiKy/DoAnCuoiKy/Service/IService/IUserBehavior/IUserReadingSessionService.cs
@@ -0,0 +1,13 @@
+﻿using DoAnCuoiKy.Model.Request.UserBookInteractionRequest;
+using DoAnCuoiKy.Model.Response;
+using DoAnCuoiKy.Model.Response.UserBehaviorResponse;
+
+namespace DoAnCuoiKy.Service.IService.IUserBehavior
+{
+    public interface IUserReadingSessionService
+    {
+        Task<BaseResponse<UserReadingSessionResponse>> CreateReadingSession(UserReadingSessionRequest readingSessionRequest);
+        Task<BaseResponse<UserReadingSessionResponse>> UpdateReadingSession(Guid sessionId, UserReadingSessionUpdateRequest updateRequest);
+        Task<BaseResponse<UserReadingSessionResponse>> GetLastPosition(Guid bookId);
+    }
+}
